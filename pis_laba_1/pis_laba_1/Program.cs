f04f11695c17b1465ef65c9a9e03b6a92fa0134c
TemlyakovPillar <egor.teml@mail.ru>
2026-09-08 19:20:46 +0300

    создана сущность MyFile

---
+++ pis_laba_1/pis_laba_1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace pis_laba_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
